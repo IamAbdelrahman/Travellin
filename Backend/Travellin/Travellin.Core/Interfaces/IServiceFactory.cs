9ad2869ea1cdf225e0bce4bc4fecfd87458c8b1a
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-18 00:32:35 +0300

    Add IServiceFactory, IIdentityFactory, IUnitOfWork

---
+++ Backend/Travellin/Travellin.Core/Interfaces/IServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿using Travellin.Travellin.Infrastructure.Services;
+
+namespace Travellin.Travellin.Core.Interfaces
+{
+    public interface IServiceFactory
+    {
+        //public IAuthTokenService AuthTokenService { get; }
+        //public IFileUploadManagementService FileUploadManagementService { get; }
+        //public IBookingManagementService BookingManagementService { get; }
+        //public IPropertyFilterExtractorService PropertyFilterExtractorService { get; }
+        //public ICheckoutManagementService CheckoutManagementService { get; }
+    }
+}
