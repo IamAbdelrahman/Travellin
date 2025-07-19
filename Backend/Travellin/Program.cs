60f600df436f422042847eccc69ff3d67bf1db67
AbdoEbid <abodaabdala19@gmail.com>
2025-07-19 16:15:26 +0300

    filters/helpers

--- Backend/Travellin/Program.cs
+++ Backend/Travellin/Program.cs
@@ -1,6 +1,9 @@
-
 using Microsoft.EntityFrameworkCore;
 using Travellin.Travellin.Infrastructure.Data;
+using Travellin.Api.Filters;
+using Travellin.Api.Helpers;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Travellin
 {
@@ -12,8 +15,26 @@ namespace Travellin
 
             // Add services to the container.
             builder.Services.AddDbContext<AirbnbDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("CFG")));
-            builder.Services.AddControllers();
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CFG")));
+
+            // Add controllers with global filters
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<ExceptionFilter>();
+                
+
+            });
+
+            
+            builder.Services.AddScoped<AuthorizeHostFilter>();
+            builder.Services.AddScoped<ExceptionFilter>();
+
+            // Configure API behavior options for consistent error responses
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -27,8 +48,10 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("CFG")));
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // Add middleware for global error handling (alternative to the filter)
+            app.UseMiddleware<ExceptionHandlerMiddleware>(); // You would need to implement this
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
---
+++ Backend/Travellin/Travellin.Api/Filters/AuthorizeHostFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travellin.Travellin.Api.Filters
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
