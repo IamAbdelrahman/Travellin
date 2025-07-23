888a2b94ed293b069beae5de1a91aaf5e95d298a
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-23 13:30:28 +0300

    feat(backend): add a controller for testing

---
+++ Backend/Travellin/Travellin.Api/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Travellin.Api.Controllers
+{
+    [Authorize(Roles ="Guest")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("Msg")]
+        public IActionResult SayHello()
+        {
+            return Ok("Hello");
+        }
+    }
+}
