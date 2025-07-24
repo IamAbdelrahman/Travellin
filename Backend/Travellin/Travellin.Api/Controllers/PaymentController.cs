a445d2c39f4e67552399ceec1b4ea8fdc7dd9425
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-24 07:24:01 +0300

    feat(backend): add payment controller

---
+++ Backend/Travellin/Travellin.Api/Controllers/PaymentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travellin.Api.Controllers;
+using Travellin.Core.Interfaces;
+
+namespace TRavellin.Api.Controllers
+{
+    public class PaymentsController : BaseController
+    {
+        IServiceFactory _serviceFactory;
+        public PaymentsController(IUnitOfWork unitOfWork, IServiceFactory serviceFactory) : base(unitOfWork)
+        {
+            _serviceFactory = serviceFactory;
+        }
+
+        [HttpPost("create-checkout-session")]
+        public async Task<IActionResult> CreateCheckoutSession([FromBody] CheckoutOptions options)
+        {
+            var result = await _serviceFactory.CheckoutManagementService.CreateCheckoutSessionAsync(options);
+            return Ok(result);
+        }
+
+        [HttpPost("stripe/webhook")]
+        public async Task<IActionResult> StripeWebHook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var signature = Request.Headers["Stripe-Signature"];
+
+            await _serviceFactory.CheckoutManagementService.HandlePaymentWebhookAsync(json, signature);
+
+            return Ok();
+        }
+    }
+}
