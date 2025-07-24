bee89e14fd7b5e899fd67b2142abe38038c0dec4
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-24 07:28:00 +0300

    feat(backend): update some repos

--- Backend/Travellin.Infrastructure/Repositories/PropertyTypeRepository.cs
+++ Backend/Travellin.Infrastructure/Repositories/PropertyTypeRepository.cs
@@ -1,5 +1,7 @@
-﻿using Travellin.Core.Entities;
+﻿using Travellin.Core.Dtos.PropertyTypes;
+using Travellin.Core.Entities;
 using Travellin.Core.Interfaces;
+using Travellin.Core.Mappings;
 using Travellin.Infrastructure.Data;
 
 namespace Travellin.Infrastructure.Repositories
@@ -8,5 +10,9 @@ namespace Travellin.Infrastructure.Repositories
     {
         public PropertyTypeRepository(TravellinDbContext dbContext) : base(dbContext)
         { }
+        public PropertyTypeDto FromEntityToDto (PropertyType entity)
+        {
+            return PropertyTypeMappingExtensions.ToDto(entity);  
+        }
     }
 }
