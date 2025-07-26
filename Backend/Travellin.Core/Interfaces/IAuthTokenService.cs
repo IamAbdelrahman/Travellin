e6cc2b61edc476741298bc55a22b338f551df0b8
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-26 06:33:25 +0300

    feat(backend) update the interface of auth service

--- Backend/Travellin.Core/Interfaces/IAuthTokenService.cs
+++ Backend/Travellin.Core/Interfaces/IAuthTokenService.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Travellin.Core.Entities;
-
+using Travellin.Travellin.Core.Enums;
 namespace Travellin.Core.Interfaces
 {
     public interface IAuthTokenService
@@ -8,5 +8,7 @@ namespace Travellin.Core.Interfaces
         public void SetAccessTokenCookie(HttpContext ctx, string token);
         public void UnsetAccessTokenCookie(HttpContext ctx);
         public string CreateToken(AppUser user);
+        public Task EnsureEntityOwnershipAsync(string entityId, string userId, string errorMsg, AuthRoles role);
+
     }
 }
