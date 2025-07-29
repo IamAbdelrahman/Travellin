6315cfc76a1eba0418be91757f4c45e4df5a88cc
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-29 10:37:40 +0300

    feat(backend): update the property photo entity

--- Backend/Travellin.Core/Entities/PropertyPhoto.cs
+++ Backend/Travellin.Core/Entities/PropertyPhoto.cs
@@ -1,9 +1,13 @@
-﻿namespace Travellin.Core.Entities
+﻿using Travellin.Core.Enums;
+
+namespace Travellin.Core.Entities
 {
     public class PropertyPhoto
     {
         public string PhotoId { get; set; }
         public string PropertyId { get; set; }
+        public string QualityStatus { get; set; } = QualityType.HighQuality.ToString(); // e.g., "HighQuality", "Blurry", "Dark", "Irrelevant"
+        public string QualityFeedback { get; set; } // e.g., "Image is blurry, please upload a clearer photo"
         public DateTime TouchedAt { get; set; }
         public virtual Property Property { get; set; }
         public virtual FileUpload FileUpload { get; set; }
