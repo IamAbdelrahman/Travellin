d0d4b399e023eeceded8dc6d69f661237d6aba86
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-29 10:38:11 +0300

    feat(backend): add pets and infants to the guest types

---
+++ Backend/Travellin.Core/Enums/QualityType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travellin.Core.Enums
+{
+    public enum QualityType
+    {
+        HighQuality,
+        Blurry,
+        Dark,
+        Irrelevant
+    }
+}
