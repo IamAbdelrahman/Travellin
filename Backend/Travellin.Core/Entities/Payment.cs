1b57e9e85db1b58b9954a9db1312dc232d09abdd
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-30 15:48:56 +0300

    feat(backend): update payment table

--- Backend/Travellin.Core/Entities/Payment.cs
+++ Backend/Travellin.Core/Entities/Payment.cs
@@ -7,11 +7,13 @@ namespace Travellin.Core.Entities
         public string BookingId { get; set; }
         public string StripeSessionId { get; set; }
         public string? StripePaymentIntentId { get; set; }
+        public string? HostStripeAccountId { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
         public PaymentStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public virtual Booking Booking { get; set; }
+
     }
 }
