d8a2985cd33d5df1c0c5355fc5287eaddced6450
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-30 15:49:28 +0300

    feat(backend): update user profile table

--- Backend/Travellin.Core/Entities/UserProfile.cs
+++ Backend/Travellin.Core/Entities/UserProfile.cs
@@ -7,10 +7,12 @@
         public string? LastName { get; set; }
         public string? Bio { get; set; }
         public int? CountryId { get; set; }
+        public string? StripeAccountId { get; set; }
         public DateOnly? BirthDate { get; set; }
         public string? PhotoId { get; set; }
         public virtual AppUser AppUser { get; set; }
         public virtual FileUpload? Photo { get; set; }
         public virtual Country? Country { get; set; }
+
     }
 }
