d45a0f9a11bf7379107297c480e19f44117643c4
Hagar-yusuf306 <hagaryusuf1@gmail.com>
2025-07-31 11:04:02 +0300

    Core

--- Backend/Travellin.Core/Interfaces/IBookingRepository.cs
+++ Backend/Travellin.Core/Interfaces/IBookingRepository.cs
@@ -6,7 +6,12 @@ namespace Travellin.Core.Interfaces
 {
     public interface IBookingRepository : IGenericRepository<Booking, string>
     {
+        //For userid get all bookings
         public Task<PaginatedResult<BookingDto>> GetByUserIdAsync(string userId, GetAllBookingsQueryParamsDto queryDto);
+        //For booking id get booking details
         public Task<BookingDto> GetBookingDetailsAsync(string bookingId);
+        //Get all bookings 
+        public Task<PaginatedResult<BookingDto>> GetAllAsync(GetAllBookingsQueryParamsDto queryDto);
+
     }
 }
